1760958f4af816985470251fac787274d88bee3c
Ranjith <birundhajayaraman2606@gmail.com>
2021-02-22 08:22:07 +0530

    Now design

--- src/component/Search/search.js
+++ src/component/Search/search.js
@@ -1,10 +1,13 @@
 import react from 'react'
 import './search.css'
 import SearchIcon from '@material-ui/icons/Search';
-import { Grid } from '@material-ui/core';
-import { Input, Space } from 'antd';
+import { Button, Grid } from '@material-ui/core';
+import { Input, Space, Select } from 'antd';
 import { AudioOutlined } from '@ant-design/icons';
 import SelectionIcon from '../../images/selectIcon.png'
+import { DownCircleTwoTone } from '@ant-design/icons'
+const { Option } = Select;
+
 
 
 const { Search } = Input;
@@ -23,6 +26,9 @@ const suffix = (
 
 function SearchPage() {
     const onSearch = value => console.log(value);
+    function handleChange(value) {
+        console.log(`selected ${value}`);
+    }
 
     return (
         <div>
@@ -39,42 +45,111 @@ function SearchPage() {
 
                 </Grid>
             </Grid>
-            <Grid spacing={1} item xs={12} container direction="column" justify="left" className="SearchInputFields">
-                <Grid spacing={2} item xs={12} container direction="row" justify="space-around" display="flex" >
+            <Grid className="SearchInputFields" spacing={2} item xs={12} container direction="column
+" >
+                <Grid spacing={1} item xs={12} container direction="row" justify="space-around" display="flex" >
                     <Grid item xs={3} container direction="row" justify="center" alignItems="right" display="flex">
-                        <Grid item xs={3} container direction="row" justify="center" className="Fields_Title">Skills</Grid>
-                        <Grid item xs={9} container justify="center" display="flex">
-                            {/* <Select
-                                suffixIcon={<img src={SelectionIcon} className="SelectInput_svg" />} showSearch placeholder="State of Domecile" optionFilterProp="children" filterOption={(input, option) =>
+                        <Grid item xs={4} container direction="row" justify="center" className="Fields_Title">Skills</Grid>
+                        <Grid item xs={8} container direction="row" justify="center" display="flex" className="selectBorder">
+
+                            <Select
+                                suffixIcon={<img src={SelectionIcon} className="Select_svg" />} showSearch optionFilterProp="children" filterOption={(input, option) =>
                                     option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
                                 }
-                                className="SelectionInput" style={{ width: "70%" }} >
+                                className="SelectInput" style={{ width: "62%", height: "22px" }} >
 
-                            </Select> */}
+                            </Select>
                         </Grid>
 
 
                     </Grid>
                     <Grid item xs={3} container direction="row" justify="left" alignItems="left" display="flex">
-                        <Grid item xs={2} container direction="row" justify="center" className="Fields_Title">Traits</Grid>
-                        <Grid item xs={10} container justify="center" display="flex">
-                            <Input />
+                        <Grid item xs={4} container direction="row" justify="center" className="Fields_Title">Traits</Grid>
+                        <Grid item xs={8} container justify="center" display="flex" className="selectBorder">
+                            <Select
+                                suffixIcon={<img src={SelectionIcon} className="Select_svg" />} showSearch optionFilterProp="children" filterOption={(input, option) =>
+                                    option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                }
+                                className="SelectInput" style={{ width: "62%", height: "22px" }} >
+
+                            </Select>
                         </Grid>
 
 
                     </Grid>
                     <Grid item xs={3} container direction="row" justify="left" alignItems="left" display="flex">
-                        <Grid item xs={2} container direction="row" justify="left" className="Fields_Title">Certification</Grid>
-                        <Grid item xs={10} container justify="center" display="flex">
-                            <Input />
+                        <Grid item xs={4} container direction="row" justify="left" className="Fields_Title">Certification</Grid>
+                        <Grid item xs={8} container justify="center" display="flex" className="selectBorder">
+                            <Select
+                                suffixIcon={<img src={SelectionIcon} className="Select_svg" />} showSearch optionFilterProp="children" filterOption={(input, option) =>
+                                    option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                }
+                                className="SelectInput" style={{ width: "62%", height: "22px" }} >
+
+                            </Select>
                         </Grid>
 
                     </Grid>
                     <Grid item xs={3} container direction="row" justify="left" display="flex">
-                        <Grid item xs={2} container justify="letf" className="Fields_Title">Achivements</Grid>
-                        <Grid item xs={10} container justify="center" display="flex">
-                            <Input />
+                        <Grid item xs={4} container justify="letf" className="Fields_Title">Achivements</Grid>
+                        <Grid item xs={8} container justify="center" display="flex" className="selectBorder">
+                            <Select
+                                suffixIcon={<img src={SelectionIcon} className="Select_svg" />} showSearch optionFilterProp="children" filterOption={(input, option) =>
+                                    option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                }
+                                className="SelectInput" style={{ width: "62%", height: "22px" }} >
+
+                            </Select>
                         </Grid>
+
+
+                    </Grid>
+
+                </Grid>
+                <Grid spacing={2} item xs={12} container direction="row" justify="space-around" display="flex" >
+
+                    <Grid item xs={4} container direction="row" justify="left" alignItems="left" display="flex">
+                        <Grid item xs={4} container direction="row" justify="center" className="Fields_Title">Specialization</Grid>
+                        <Grid item xs={8} container justify="center" display="flex" className="selectBorder">
+                            <Select
+                                suffixIcon={<img src={SelectionIcon} className="Select_svg" />} showSearch optionFilterProp="children" filterOption={(input, option) =>
+                                    option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                }
+                                className="SelectInput" style={{ width: "62%", height: "22px" }} >
+
+                            </Select>
+                        </Grid>
+
+
+                    </Grid>
+                    <Grid item xs={4} container direction="row" justify="left" alignItems="left" display="flex">
+                        <Grid item xs={4} container direction="row" justify="left" className="Fields_Title">Capabilities</Grid>
+                        <Grid item xs={8} container justify="center" display="flex" className="selectBorder">
+                            <Select
+                                suffixIcon={<img src={SelectionIcon} className="Select_svg" />} showSearch optionFilterProp="children" filterOption={(input, option) =>
+                                    option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                }
+                                className="SelectInput" style={{ width: "62%", height: "22px" }} >
+
+                            </Select>
+                        </Grid>
+
+                    </Grid>
+                    <Grid item xs={3} container direction="row" justify="left" display="flex">
+                        <Grid item xs={4} container justify="letf" className="Fields_Title">Talents</Grid>
+                        <Grid item xs={8} container justify="center" display="flex" className="selectBorder">
+                            <Select
+                                suffixIcon={<img src={SelectionIcon} className="Select_svg" />} showSearch optionFilterProp="children" filterOption={(input, option) =>
+                                    option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                }
+                                className="SelectInput" style={{ width: "62%", height: "22px" }} >
+
+                            </Select>
+                        </Grid>
+
+                    </Grid>
+                    <Grid item xs={1} container direction="row" justify="left" display="flex">
+                        <Button>Save</Button>
 
                     </Grid>
 
